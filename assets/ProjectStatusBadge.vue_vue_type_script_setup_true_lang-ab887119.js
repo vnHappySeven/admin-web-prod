f29b142997ae5a10a2c471d7ef5e633ed9323c77
Parsons <gongyingparsons@gmail.com>
2025-02-13 22:55:06 +0800

    deploy

---
+++ assets/ProjectStatusBadge.vue_vue_type_script_setup_true_lang-ab887119.js
@@ -0,0 +1,2 @@
+import{d as t,f as s,a,g as p}from"./index-5ba5c4ff.js";const d=t({__name:"ProjectStatusBadge",props:{status:{type:String,required:!0}},setup(n){const o={"in progress":"primary",archived:"secondary",completed:"success",important:"warning"};return(e,c)=>{const r=s("VaBadge");return a(),p(r,{square:"",color:o[e.$props.status],text:e.$props.status.toUpperCase()},null,8,["color","text"])}}});export{d as _};
+//# sourceMappingURL=ProjectStatusBadge.vue_vue_type_script_setup_true_lang-ab887119.js.map
