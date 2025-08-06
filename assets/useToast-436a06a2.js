42236967e0cc7cb860f9f173ef932001b3d2bfbd
Parsons <gongyingparsons@gmail.com>
2025-08-06 22:10:15 +0800

    deploy

---
+++ assets/useToast-436a06a2.js
@@ -0,0 +1,2 @@
+import{L as c,e as l,a9 as r,aa as u,ab as a,ac as i}from"./index-be8890b2.js";const p=()=>{const e=c();return l(()=>{var o;return((o=r())==null?void 0:o._context)||(e==null?void 0:e.appContext)})},v=()=>{const e=p(),o=[],s=t=>{const n=u(t,e.value);return n&&o.push(n),n};return{init:t=>s(t),notify:s,close:t=>a(t),closeAll:(t=!1)=>i(t?void 0:e.value),closeAllCreatedInThisHook:()=>{o.forEach(t=>a(t))}}};export{v as u};
+//# sourceMappingURL=useToast-436a06a2.js.map
