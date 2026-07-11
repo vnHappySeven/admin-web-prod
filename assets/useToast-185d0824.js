892271873ea4be2a683c1b9b020121b1957d17fc
Parsons <gongyingparsons@gmail.com>
2026-07-12 01:28:29 +0800

    deploy

--- assets/useToast-185d0824.js
+++
@@ -1,2 +0,0 @@
-import{M as c,e as l,ad as r,ae as u,af as a,ag as i}from"./index-bee7149b.js";const p=()=>{const e=c();return l(()=>{var o;return((o=r())==null?void 0:o._context)||(e==null?void 0:e.appContext)})},v=()=>{const e=p(),o=[],s=t=>{const n=u(t,e.value);return n&&o.push(n),n};return{init:t=>s(t),notify:s,close:t=>a(t),closeAll:(t=!1)=>i(t?void 0:e.value),closeAllCreatedInThisHook:()=>{o.forEach(t=>a(t))}}};export{v as u};
-//# sourceMappingURL=useToast-185d0824.js.map
